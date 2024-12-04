5ffc6f3580102a3d86b6f20c9e0f451a5b366dac
Grande Variable <gvariable@icloud.com>
2024-12-04 20:58:06 +0100

    initial commit

--- server.js
+++ server.js
@@ -3,17 +3,17 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
 // For any other routes, serve the index.html
-app.get('*', (req, res) => {
+app.use('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Set the port
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
