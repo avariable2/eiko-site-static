f904e769d20ae70497d284d2707c744314c068d4
Grande Variable <gvariable@icloud.com>
2024-12-07 18:04:05 +0100

    modifie server.js pour permettre la récupération du fichier depuis la racine

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
-// server.js
-
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/.well-known/apple-app-site-association', (req, res) => {
-    res.type('application/json');
-    res.sendFile(path.join(__dirname, 'public', '.well-known', 'apple-app-site-association'));
-  });  
+// Route spécifique pour apple-app-site-association
+app.get('/apple-app-site-association', (req, res) => {
+  const filePath = path.join(__dirname, 'apple-app-site-association');
+  
+  // Vérifiez si le fichier existe
+  fs.access(filePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      console.error('Fichier AASA non trouvé:', err);
+      return res.status(404).send('Not Found');
+    }
+    console.log('Fichier AASA trouvé');
+    res.setHeader('Content-Type', 'application/json');
+    res.sendFile(filePath);
+  });
+});
 
 // For any other routes, serve the index.html
 app.use('/', (req, res) => {
