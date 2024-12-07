72c09961d313de150ef99a0d0045ec0c968d8921
Grande Variable <gvariable@icloud.com>
2024-12-07 17:14:02 +0100

    Configure apple-app-site-association

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ const PORT = process.env.PORT || 3000;
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/.well-known/apple-app-site-association', (req, res) => {
+    res.type('application/json');
+    res.sendFile(path.join(__dirname, 'public', '.well-known', 'apple-app-site-association'));
+  });  
+
 // For any other routes, serve the index.html
 app.use('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
